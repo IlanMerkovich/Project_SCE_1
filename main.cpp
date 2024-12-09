24db22ffc6775804c6ab48d17048f4dc92ea2aea
Ilan Merkovich <merkovichilan@gmail.com>
2024-12-09 21:55:30 +0200

    added registration

--- main.cpp
+++ main.cpp
@@ -6,30 +6,50 @@
 #include "fstream"
 #include "string"
 #include "vector"
+#include "limits"
+#define PATIENTSFILE "Patients.txt"
+#define DOCTORSFILE "Doctors.txt"
 using namespace std;
 
 //vector<Patient> patients = Patient::readFromFile(filename);
 //vector<Doctor> doctors = Doctor::readFromFile(filename);
 
-int validate_register_data_patient(string Health_provider,long id){
+bool validate_id(long long id){
     if (id==0 || to_string(id).length()!=9)
-        return 1;
-    if (Health_provider!="Leumit" || Health_provider!="Macabi" || Health_provider!="Meuhedet" || Health_provider!="Clalit")
-        return 2;
-    return 0;
+        return false;
+    return true;
 }
-bool Register_patient(string Name,string Phone_number,string Health_provider,long id,long password){
-    cout<<"=========================="<<endl;
+Patient Register_patient(){
+    string Name,Phone_number,Health_provider;
+    long long id,password;
+    cout<<endl;
+    cout<<"=====================[Patient Registration]====================="<<endl;
     cout<<"System is ready to start registration process!"<<endl;
-
+    cout<<"Please Enter you full name:"<<endl;
+    getline(cin,Name);
+    do {
+        cout<<"Please Enter your id number(9-Digits number):"<<endl;
+        cin >> id;
+        if (!validate_id(id)) {
+            cout << "Invalid ID. Please try again." << endl;
+        }
+    } while (!validate_id(id));
+    cout<<"Please Enter your health care provider:"<<endl;
+    cin>>Health_provider;
+    cout<<"Please Enter your phone number:"<<endl;
+    cin>>Phone_number;
+    cout<<"Enter a strong password for you account (password must consist of numbers only):"<<endl;
+    cin>>password;
+    cout<<"Registration is OK!"<<endl;
+    cout<<"===================[Patient Registration Completed!]==================="<<endl;
+    Patient returned_patient(Name,id,Phone_number,password,Health_provider);
+    returned_patient.saveToFile(PATIENTSFILE);
+    return returned_patient;
 }
 
 
 int main(){
-    string name,phone_number,health_care;
-    long id,pass;
-    getline(cin,name);
-    cout<<name;
-    cin>>id;
-    cout<<id;
+    vector<Patient>Patients;
+    Patients.push_back(Register_patient());
+    cout<<Patients[0].get_type();
 }
